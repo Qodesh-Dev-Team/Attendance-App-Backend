1ab67c289fdfd2c763f03181206d42b648e47d7a
Young Plutus <malicksamah3@gmail.com>
2025-01-19 21:04:21 +0000

    first commit

---
+++ src/main/java/com/qcc/hallow/HallowApplication.java
@@ -0,0 +1,13 @@
+package com.qcc.hallow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HallowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HallowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qcc/hallow/HallowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qcc.hallow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HallowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
